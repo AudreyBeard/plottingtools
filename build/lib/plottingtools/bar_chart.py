01213dc1f299f2b2da097fead24579b325edbb3f
joshuabeard <joshuabeard92@gmail.com>
2019-01-08 13:47:00 -0500

    fixed some stuff and extended functionality

--- build/lib/plottingtools/bar_chart.py
+++ build/lib/plottingtools/bar_chart.py
@@ -8,18 +8,20 @@ from matplotlib import pyplot as plt
 def plot_separate(data,
                   labels=None,
                   bar_width=0.75,
-                  figsize=(16, 8),
+                  figsize=(12, 8),
                   title='',
                   ylim=None,
                   max_val_pad=None,
                   sort_order=None,
                   use_bottom_labels=False,
-                  scale_data=False,
                   use_legend=False,
                   show_max_val=False,
                   show=True,
                   save=False,
                   save_name=None,
+                  scale_by=None,
+                  label_bars=None,
+                  label_bars_format='percent',
                   ):
 
     string_dtype = np.dtype('<U1')
@@ -48,8 +50,8 @@ def plot_separate(data,
     else:
         bottom_labels = None
 
-    if scale_data:
-        data = data / n_total
+    if scale_by is not None:
+        data = data / scale_by
         n_total = 1
 
     # Separation between bars
@@ -72,9 +74,26 @@ def plot_separate(data,
                 '--',
                 color=[0.75, 0.75, 0.75])
 
+    if label_bars is not None:
+
+        if label_bars_format.lower() == 'percent':
+            def fmt(x):
+                return '{:.1f}%'.format(x * 100)
+        else:
+            def fmt(x):
+                return '{}'.format(x)
+
+        if label_bars.lower() == 'above':
+            if scale_by is not None:
+                y_offset = 0.05
+
+        for i in range(len(labels)):
+            plt.text(x=offsets[i], y=data[i] + y_offset, s=fmt(data[i]))
+
     if ylim is None:
         if max_val_pad is None:
-            max_val_pad = 0
+            max_val_pad = .1
+
         ylim = (0, max(data) + max(data) * max_val_pad)
 
     ax.set_ylim(ylim)
@@ -91,11 +110,11 @@ def plot_separate(data,
     fig.suptitle(title, fontsize=max(figsize) * 2)
     plt.tick_params(labelsize=max(figsize))
 
-    if show:
-        plt.show()
-
     if save:
         if save_name is not None:
             plt.savefig(save_name)
         else:
             plt.savefig(title)
+
+    if show:
+        plt.show()
