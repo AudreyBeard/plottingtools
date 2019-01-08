8d57e4543b49788be2093f62ffae9e247a3feea6
joshuabeard <joshuabeard92@gmail.com>
2019-01-08 11:37:15 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='plottingtools',
+      version='0.0',
+      description='A collection of tools for plotting graphs that I have used often enough (or are complex enough) for me to add to a repo',
+      author='Joshua Beard',
+      author_email='joshuabeard92@gmail.com',
+      packages=['plottingtools'],
+      install_requires=['numpy', 'matplotlib'],
+      )
