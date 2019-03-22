1e5728356e9e27f332b9d14a1c7bca29c4ca3cd0
Josh Beard <joshuabeard92@gmail.com>
2019-03-22 17:10:05 -0400

    simple lines example - need to test

---
+++ plottingtools/examples/lines/simple.py
@@ -0,0 +1,15 @@
+import plottingtools as pt
+import numpy as np
+
+
+length = 25
+x = np.arange(length)
+y = np.arange(length) - np.random.randint(length, size=length)
+
+kwargs = dict(x=x,
+              y=y,
+              title='Witty Title',
+              )
+
+if __name__ == "__main__":
+    plot = pt.plot.Plot2D(**kwargs)
